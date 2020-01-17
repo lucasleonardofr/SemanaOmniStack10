b342a89e40fed502d5947d5da77a134624d5c66d
lucasleonardofr <lucasleonardofr@gmail.com>
2020-01-17 09:59:42 -0300

    Adiçao das funcionalidades de realtime no backend usando socketio e atualizaçao na aplicaçao mobile

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
   async index(request, response) {
@@ -36,14 +37,14 @@ module.exports = {
         techs: techsArray,
         location
       });
+      //Filtrar as  conexões que estão há 10Km de distância
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      );
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
 
     return response.json(dev);
-  },
-
-  //Atualizar um unico dev
-  async update() {},
-
-  //deletar um dev do bd
-  async destroy() {}
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
+
 const routes = require("./routes");
+const { setupWebsocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(
   "mongodb+srv://lucasleonardofr:omnistack@cluster0-dgax8.mongodb.net/week10?retryWrites=true&w=majority",
@@ -19,4 +25,4 @@ app.use(express.json());
 app.use(routes);
 
 //definindo a porta do localhost
-app.listen(3333);
+server.listen(3333);
